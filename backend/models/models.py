2dd3a080318d7faf2df47c29a37f4d09335ed246
Prasad-Kedar <prasadkedar04@gmail.com>
2026-07-15 21:22:47 +0530

    feat(models): add Device model

--- backend/models/models.py
+++ backend/models/models.py
@@ -2,17 +2,23 @@ from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
 from database.db import Base
 
+
 class Firmware(Base):
     __tablename__ = "firmware"
 
     id = Column(Integer, primary_key=True, index=True)
-
     firmware_name = Column(String, nullable=False)
-
     version = Column(String, nullable=False)
-
     hash = Column(String, nullable=False)
-
     signature = Column(String, nullable=False)
-
     uploaded_at = Column(DateTime, default=datetime.utcnow)
+
+
+class Device(Base):
+    __tablename__ = "devices"
+
+    id = Column(Integer, primary_key=True, index=True)
+    device_name = Column(String, nullable=False)
+    serial_number = Column(String, unique=True, nullable=False)
+    current_version = Column(String, nullable=False)
+    status = Column(String, default="ACTIVE")
