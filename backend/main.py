4eb2a43408d3adf30ee9f6e6887ce169b246ff42
Prasad-Kedar <prasadkedar04@gmail.com>
2026-07-15 21:34:20 +0530

    feat(database): initialize database tables

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 
+from database.db import Base, engine
+from models.models import Firmware, Device, UpdateHistory
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
     title="Secure Firmware Update System"
 )
