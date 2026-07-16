227d19078dbb515193f7d75cb40a9fa75938ed2d
Prasad-Kedar <prasadkedar04@gmail.com>
2026-07-16 21:02:32 +0530

    feat(storage): save uploaded firmware files

--- backend/routers/firmware.py
+++ backend/routers/firmware.py
@@ -1,16 +1,21 @@
 from fastapi import APIRouter, UploadFile, File, Form
+import shutil
+import os
 
 router = APIRouter()
-
 @router.post("/firmware/upload")
 async def upload_firmware(
     firmware: UploadFile = File(...),
     version: str = Form(...),
     firmware_name: str = Form(...)
 ):
+
+    file_path = f"uploads/{firmware.filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(firmware.file, buffer)
+
     return {
-        "filename": firmware.filename,
-        "version": version,
-        "firmware_name": firmware_name,
-        "message": "Firmware received successfully"
+        "message": "Firmware uploaded successfully",
+        "filename": firmware.filename
     }
