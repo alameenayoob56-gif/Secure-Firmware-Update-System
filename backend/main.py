6293541ad2a6ad8d55848fa89f1701cf12c75ef0
Prasad-Kedar <prasadkedar04@gmail.com>
2026-07-21 19:01:11 +0530

    feat(api): add JWT login endpoint

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,13 @@ from fastapi import FastAPI
 from database.db import Base, engine
 from models.models import Firmware, Device
 from routers.firmware import router as firmware_router
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi import Depends, HTTPException
+from pydantic import BaseModel
+from utils.auth_utils import create_access_token, verify_token
 
 app = FastAPI(
+    security = HTTPBearer()
     title="Secure Firmware Update System"
 )
 
@@ -17,4 +22,30 @@ app.include_router(firmware_router)
 def root():
     return {
         "message": "Secure Firmware Update System API"
+    }
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+    @app.post("/login")
+def login(request: LoginRequest):
+
+    user = fake_users.get(request.username)
+
+    if not user or user["password"] != request.password:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username or password"
+        )
+
+    token = create_access_token({
+        "sub": user["username"],
+        "role": user["role"]
+    })
+
+    return {
+        "access_token": token,
+        "token_type": "bearer",
+        "role": user["role"]
     }
